9f5345d42d75bbd5ff59f4fa542b2ae308ff93f7
Hossein <hossienbehzadi1381@gmail.com>
2024-02-25 19:38:08 +0330

    move the ball

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pygame
 import sys
 
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -16,11 +30,16 @@ ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
 
+ball_speed_x = 7
+ball_speed_y = 7
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball_animation()
 
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)

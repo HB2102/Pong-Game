a67388c2c2c1952f3d0b674d2c6c9bc36b4b9e7f
Hossein <hossienbehzadi1381@gmail.com>
2024-02-25 19:42:03 +0330

    add opponent and restart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 import sys
 
 
@@ -7,12 +8,43 @@ def ball_animation():
 
     ball.x += ball_speed_x
     ball.y += ball_speed_y
+
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+        ball_start()
+
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
+
+
+def player_animation():
+    player.y += player_speed
+
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.y += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.y -= opponent_speed
+
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+
+def ball_start():
+    global ball_speed_x, ball_speed_y
+
+    ball.center = (screen_width / 2, screen_height / 2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
 
 
 pygame.init()
@@ -30,16 +62,30 @@ ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
 
-ball_speed_x = 7
-ball_speed_y = 7
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
+player_speed = 0
+opponent_speed = 7
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player_speed -= 6
+            if event.key == pygame.K_DOWN:
+                player_speed += 6
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player_speed += 6
+            if event.key == pygame.K_DOWN:
+                player_speed -= 6
 
     ball_animation()
+    player_animation()
+    opponent_ai()
 
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
